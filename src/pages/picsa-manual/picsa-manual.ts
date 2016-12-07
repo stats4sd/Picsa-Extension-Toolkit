05dde454653c6d252a1b4c76a51e2015d8e442e6
chrismclarke <chris.m.clarke@live.co.uk>
2016-12-07 15:56:35 +0000

    Initial Commit

---
+++ src/pages/picsa-manual/picsa-manual.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the PicsaManual page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-picsa-manual',
+  templateUrl: 'picsa-manual.html'
+})
+export class PicsaManualPage {
+  chapters:any;
+
+  constructor(public navCtrl: NavController) {
+    this.chapters={
+      
+    }
+  }
+
+  ionViewDidLoad() {
+    console.log('Hello PicsaManualPage Page');
+  }
+
+}
