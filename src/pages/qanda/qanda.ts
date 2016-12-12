bc3b47093c7deebbe0651728b25ddb337fbb9082
CHRIS-DESKTOP\Chris <chris.m.clarke@live.co.uk>
2016-12-12 12:58:01 +0000

    q-amd-a page and firebase integration

---
+++ src/pages/qanda/qanda.ts
@@ -0,0 +1,112 @@
+import { Component } from '@angular/core';
+
+import {NavController, AlertController, ActionSheetController} from 'ionic-angular';
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
+
+@Component({
+  selector: 'page-qanda',
+  templateUrl: 'qanda.html'
+})
+export class QandAPage {
+  songs: FirebaseListObservable<any>;
+
+  constructor(public navCtrl: NavController, af:AngularFire, public alertCtrl:AlertController, public actionSheetCtrl: ActionSheetController) {
+    this.songs=af.database.list('/songs');
+    console.log(this.songs)
+
+
+  }
+
+  addSong(){
+    let prompt = this.alertCtrl.create({
+      title: 'Song Name',
+      message: "Enter a name for this new song you're so keen on adding",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.songs.push({
+              title: data.title
+            });
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  showOptions(songId, songTitle) {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'What do you want to do?',
+      buttons: [
+        {
+          text: 'Delete Song',
+          role: 'destructive',
+          handler: () => {
+            this.removeSong(songId);
+          }
+        },{
+          text: 'Update title',
+          handler: () => {
+            this.updateSong(songId, songTitle);
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  removeSong(songId: string){
+    this.songs.remove(songId);
+  }
+
+  updateSong(songId, songTitle){
+    let prompt = this.alertCtrl.create({
+      title: 'Song Name',
+      message: "Update the name for this song",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title',
+          value: songTitle
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.songs.update(songId, {
+              title: data.title
+            });
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+}
