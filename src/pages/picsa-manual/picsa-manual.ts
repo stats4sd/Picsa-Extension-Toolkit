cb1560cd18bdf946975cca03087fa5339230a58d
CHRIS-DESKTOP\Chris <chris.m.clarke@live.co.uk>
2017-06-12 09:22:37 +0100

    rework pdf component

--- src/pages/picsa-manual/picsa-manual.ts
+++ src/pages/picsa-manual/picsa-manual.ts
@@ -1,58 +1,53 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 
-/*
-  Generated class for the PicsaManual page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-picsa-manual',
   templateUrl: 'picsa-manual.html',
 
 })
-export class PicsaManualPage {
-  chapters:any;
-  pdfjsframe:any;
-  fileBase64:any;
-  isPdf:boolean=true;
 
-  constructor(public navCtrl: NavController) {
-    this.chapters={
-      
-    }
+export class PicsaManualPage {
+  chapters: any;
+  pdfjsframe: any;
+  fileBase64: any;
+  isPdf: boolean = true;
+  pdfSrc: string;
+  page: number;
+  loader: any;
+  pdfIsLoaded: boolean = false;
+
+  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController) {
+
   }
+  ionViewDidEnter() {
+    console.log('ion page entered')
+    if (!this.pdfIsLoaded) {
+      console.log('loading pdf')
+      this.pdfSrc = "assets/picsa-field-manual.pdf"
+      this.page = 1
+      this.loader = this.loadingCtrl.create({
+        content: "Loading...",
+        spinner: "dots"
+      });
+      this.loader.present();
+      console.log('loader presented')
+    }
+    console.log('pdf already loaded')
+
+  }
+
 
   ionViewDidLoad() {
-    console.log('Hello PicsaManualPage Page');
-    console.log('test 2')
+    console.log('ion view loaded')
+  }
+  pdfLoaded() {
+    console.log('pdf loaded')
+    this.loader.dismiss()
   }
 
-  openPdf(): void {
-    var _self = this;
-    setTimeout(() => {
-      this.pdfjsframe = document.getElementById('pdfViewer');
-      if (this.pdfjsframe != null) {
-        this.pdfjsframe.onload = function () {
-          _self.loadPdfDocument();
-        };
-      }
-    }, 0);
-  }
 
-  loadPdfDocument() {
-    var pdfData = this.base64ToUint8Array(this.fileBase64);
-    this.pdfjsframe.contentWindow.PDFViewerApplication.open(pdfData);
-  }
 
-  base64ToUint8Array(base64) {
-    var raw = atob(base64);
-    var uint8Array = new Uint8Array(raw.length);
-    for (var i = 0; i < raw.length; i++) {
-      uint8Array[i] = raw.charCodeAt(i);
-    }
-    return uint8Array;
-  }
+
 
 }
