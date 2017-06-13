73ad0f7802573c20e38e4878578ad4eb5c7f80c1
chris <chris.m.clarke@live.co.uk>
2017-06-13 16:12:48 +0100

    pdf rollback

--- src/pages/picsa-manual/picsa-manual.ts
+++ src/pages/picsa-manual/picsa-manual.ts
@@ -1,56 +1,79 @@
 import { Component } from '@angular/core';
-import { NavController, LoadingController } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
+import * as pdfjsLib from 'pdfjs-dist';
 
+/*
+  Generated class for the PicsaManual page.
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
 @Component({
   selector: 'page-picsa-manual',
   templateUrl: 'picsa-manual.html',
 
 })
-
 export class PicsaManualPage {
   chapters: any;
   pdfjsframe: any;
   fileBase64: any;
   isPdf: boolean = true;
-  pdfSrc: string;
-  page: number;
-  loader: any;
-  pdfIsLoaded: boolean = false;
+  canvas: any;
 
-  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController) {
+  constructor(public navCtrl: NavController) {
+    // pdfjsLib.PDFJS.workerSrc = 'assets/pdf.worker.js';
 
   }
-  ionViewDidEnter() {
-    console.log('ion page entered')
-    if (!this.pdfIsLoaded) {
-      console.log('loading pdf')
-      this.pdfSrc = "assets/picsa-field-manual.pdf"
-      this.page = 1
-      this.loader = this.loadingCtrl.create({
-        content: "Loading...",
-        spinner: "dots"
-      });
-      this.loader.present();
-      console.log('loader presented')
-    }
-    console.log('pdf already loaded')
-
-  }
-
 
   ionViewDidLoad() {
-    console.log('ion view loaded')
+    console.log('Hello PicsaManualPage Page');
+    console.log('test 2')
+    // var data = new Uint8Array(fs.readFileSync('helloworld.pdf'));
+  //   PDFJS.getDocument('assets/picsa-field-manual.pdf').then(function (pdfDocument) {
+  //     console.log('Number of pages: ' + pdfDocument.numPages);
+  //     return pdfDocument.getPage(1).then(function (pdfPage) {
+  //       // Display page on the existing canvas with 100% scale.
+  //       var viewport = pdfPage.getViewport(1.0);
+  //       this.canvas = document.getElementById('pdfCanvas');
+  //       this.canvas.width = viewport.width;
+  //       this.canvas.height = viewport.height;
+  //       var ctx = this.canvas.getContext('2d');
+  //       var renderTask = pdfPage.render({
+  //         canvasContext: ctx,
+  //         viewport: viewport
+  //       });
+  //       return renderTask.promise;
+  //     });
+  //   }).catch(function (reason) {
+  //     console.error('Error: ' + reason);
+  //   });
+  // });
+
+}
+
+openPdf(): void {
+  var _self = this;
+  setTimeout(() => {
+  this.pdfjsframe = document.getElementById('pdfViewer');
+  if (this.pdfjsframe != null) {
+    this.pdfjsframe.onload = function () {
+      _self.loadPdfDocument();
+    };
   }
-  pinchZoom(e){
-    console.log('pinch zoomed',e)
-  }
-  pdfLoaded() {
-    console.log('pdf loaded')
-    this.loader.dismiss()
+}, 0);
   }
 
+loadPdfDocument() {
+  var pdfData = this.base64ToUint8Array(this.fileBase64);
+  this.pdfjsframe.contentWindow.PDFViewerApplication.open(pdfData);
+}
 
-
-
+base64ToUint8Array(base64) {
+  var raw = atob(base64);
+  var uint8Array = new Uint8Array(raw.length);
+  for (var i = 0; i < raw.length; i++) {
+    uint8Array[i] = raw.charCodeAt(i);
+  }
+  return uint8Array;
+}
 
 }
