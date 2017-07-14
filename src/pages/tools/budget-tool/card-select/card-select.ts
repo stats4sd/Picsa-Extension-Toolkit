622f1e24a08d09ecf1ecab6ffb58ba7a9a792e5c
chris <chris.m.clarke@live.co.uk>
2017-07-14 19:30:22 +0200

    more building out budget tool interactions

--- src/pages/tools/budget-tool/card-select/card-select.ts
+++ src/pages/tools/budget-tool/card-select/card-select.ts
@@ -15,70 +15,81 @@ export class CardSelectPage {
   showCards = false
   showValues = false;
   showFamilyLabour = false;
-  type:string
+  showConsumed = false;
+  type: string
   period: any;
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public bdg: BudgetToolProvider,
-    public viewCtrl:ViewController) {
+    public viewCtrl: ViewController) {
     this.cards = this.bdg.allData[this.navParams.data.type]
-    this.period=this.navParams.data.period
+    this.period = this.navParams.data.period
 ***REMOVED***
   ionViewDidEnter() {
     this.setSelected(this.navParams.data.selected || [])
-    console.log('period',this.navParams.data.period)
+    console.log('period', this.navParams.data.period)
     this.type = this.navParams.data.type
-    this.showValues = (this.type == "inputs" || this.type == "outputs")
     this.showFamilyLabour = (this.type == "familyLabour")
-    this.showCards = (this.type != "familyLabour")
-    
+    this.showCards = (this.type != "familyLabour" && this.type !="consumed")
+    this.showConsumed = (this.type == "consumed")
+
 ***REMOVED***
 
   highlight(card) {
+    //push card if doesn't exist
     if (!this.selected[card.ID]) {
+      console.log('adding card', card)
       this.selected[card.ID] = card
-      if (!this.period[this.type]) { this.period[this.type]=[]}
+      if (!this.period[this.type]) { this.period[this.type] = [] }
       this.period[this.type].push(card)
-      console.log('period',this.period)
-      if (card.Type == "input" || card.Type == "output") {
-        if (!card.quantity) { card.quantity = 0 }
-        if (!card.cost) { card.cost = 0 }
-        this.activeCard = card;  
+      console.log('period', this.period)
+  ***REMOVED***
+    //else remove or toggle values
+    else {
+      if (card.Type != "input" && card.Type != "output") {
+        console.log('removing card', card)
+        this.removeCard(card)
     ***REMOVED***
   ***REMOVED***
-    else {
-      delete this.selected[card.ID]
-      console.log('filtering list', this.period[this.type])
-      this.period[this.type]=this.period[this.type].filter(function (element) {
-        console.log('id',element.ID,card.ID)
-        return element.ID !=card.ID
-    ***REMOVED***)
-      console.log('period',this.period)
+    //if input/output toggle values pane
+    if (card.Type == "input" || card.Type == "output") {
+      if (!card.quantity) { card.quantity = 0 }
+      if (!card.cost) { card.cost = 0 }
+      this.activeCard = this.selected[card.ID] || card
+      this.showValues = true;
   ***REMOVED***
+***REMOVED***
+
+  removeCard(card) {
+    delete this.selected[card.ID]
+    this.period[this.type] = this.period[this.type].filter(function (element) {
+      return element.ID != card.ID
+  ***REMOVED***)
+    this.showValues = false;
 ***REMOVED***
   increaseFamily(variable) {
     if (this.period.familyLabour[variable] >= 0) {
       this.period.familyLabour[variable]++
-  ***REMOVED***  
+  ***REMOVED***
 ***REMOVED***
   decreaseFamily(variable) {
     if (this.period.familyLabour[variable] >= 0) {
-      this.period.familyLabour[variable] --
+      this.period.familyLabour[variable]--
   ***REMOVED***
 ***REMOVED***
   _toArray(value) {
     return new Array(value).fill(0)
 ***REMOVED***
 
-  
+
 
   setSelected(array) {
     console.log('setting selected')
     for (let item of array) {
       this.selected[item.ID] = item;
   ***REMOVED***
-   console.log('selected',this.selected) 
+    console.log('selected', this.selected)
 ***REMOVED***
   updateValues(card) {
     console.log('updating values', card)
@@ -88,7 +99,7 @@ export class CardSelectPage {
   close() {
     //repopulate 
     this.viewCtrl.dismiss(this.period)
-    
+
 ***REMOVED***
 
 }
--- src/providers/budget-tool/budget-tool.ts
+++ src/providers/budget-tool/budget-tool.ts
@@ -18,10 +18,12 @@ export class BudgetToolProvider {
           { "Type": "activity", "Name": "land clearing", "Image": "assets/img/budget/activity/land-clearing.png", "ID": "land-clearing" },
         ],
         inputs: [
-          { "Type": "input", "Name": "manure wheelbarrows", "Image": "assets/img/budget/input/manure-wheelbarrows.png", "ID": "manure-wheelbarrows", quantity: 5, cost: 200 },
-          { "Type": "input", "Name": "seeds", "Image": "assets/img/budget/input/seeds.png", "ID": "seeds" , quantity:1, cost:500},
+          { "Type": "input", "Name": "manure wheelbarrows", "Image": "assets/img/budget/input/manure-wheelbarrows.png", "ID": "manure-wheelbarrows", quantity: 5, cost: 200, consumed:0 },
+          { "Type": "input", "Name": "seeds", "Image": "assets/img/budget/input/seeds.png", "ID": "seeds" , quantity:1, cost:500, consumed:0},
         ],
-        outputs: [],
+        outputs: [
+          { "Type": "output", "Name": "crop", "Image": "assets/img/budget/output/crop.png", "ID": "crop", quantity: 6, cost:200, consumed: 1 },
+        ],
         familyLabour: {
           people: 3,
           days:4
@@ -94,8 +96,6 @@ var allData = {
     { "Type": "activity", "Name": "land clearing", "Image": "assets/img/budget/activity/land-clearing.png", "ID": "land-clearing" },
     { "Type": "activity", "Name": "marketing and selling", "Image": "assets/img/budget/activity/marketing-and-selling.png", "ID": "marketing-and-selling" },
     { "Type": "activity", "Name": "mulching", "Image": "assets/img/budget/activity/mulching.png", "ID": "mulching" },
-    // { "Type": "activity", "Name": "none", "Image": "assets/img/budget/activity/none.png", "ID": "none" },
-    // { "Type": "activity", "Name": "other", "Image": "assets/img/budget/activity/other.png", "ID": "other" },
     { "Type": "activity", "Name": "ploughing", "Image": "assets/img/budget/activity/ploughing.png", "ID": "ploughing" },
     { "Type": "activity", "Name": "shelling", "Image": "assets/img/budget/activity/shelling.png", "ID": "shelling" },
     { "Type": "activity", "Name": "sowing", "Image": "assets/img/budget/activity/sowing.png", "ID": "sowing" },
@@ -113,8 +113,6 @@ var allData = {
     { "Type": "input", "Name": "labour - paid", "Image": "assets/img/budget/input/labour---paid.png", "ID": "labour---paid" },
     { "Type": "input", "Name": "manure sacks", "Image": "assets/img/budget/input/manure-sacks.png", "ID": "manure-sacks" },
     { "Type": "input", "Name": "manure wheelbarrows", "Image": "assets/img/budget/input/manure-wheelbarrows.png", "ID": "manure-wheelbarrows" },
-    { "Type": "input", "Name": "none", "Image": "assets/img/budget/input/none.png", "ID": "none" },
-    { "Type": "input", "Name": "other", "Image": "assets/img/budget/input/other.png", "ID": "other" },
     { "Type": "input", "Name": "pot for storage", "Image": "assets/img/budget/input/pot-for-storage.png", "ID": "pot-for-storage" },
     { "Type": "input", "Name": "protective equipment", "Image": "assets/img/budget/input/protective-equipment.png", "ID": "protective-equipment" },
     { "Type": "input", "Name": "seeds", "Image": "assets/img/budget/input/seeds.png", "ID": "seeds" },
@@ -126,12 +124,9 @@ var allData = {
   ],
   outputs: [
     { "Type": "labour", "Name": "family labour", "Image": "assets/img/budget/labour/family-labour.png", "ID": "family-labour" },
-    { "Type": "labour", "Name": "none", "Image": "assets/img/budget/labour/none.png", "ID": "none" },
     { "Type": "output", "Name": "crop", "Image": "assets/img/budget/output/crop.png", "ID": "crop" },
     { "Type": "output", "Name": "manure for compost", "Image": "assets/img/budget/output/manure-for-compost.png", "ID": "manure-for-compost" },
     { "Type": "output", "Name": "money", "Image": "assets/img/budget/output/money.png", "ID": "money" },
-    { "Type": "output", "Name": "none", "Image": "assets/img/budget/output/none.png", "ID": "none" },
-    { "Type": "output", "Name": "other", "Image": "assets/img/budget/output/other.png", "ID": "other" },
     { "Type": "output", "Name": "wood", "Image": "assets/img/budget/output/wood.png", "ID": "wood" }
   ]
 }
