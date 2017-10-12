bce2cebae9b0ceb4fdae7d7d6e0a8f452b8dce3e
CHRIS-DESKTOP\Chris <chris.m.clarke@live.co.uk>
2017-10-12 02:02:50 +0100

    feedback and monitoring form building

--- src/pages/record-data/forms/feedback-form/feedback-form.ts
+++ src/pages/record-data/forms/feedback-form/feedback-form.ts
@@ -1,12 +1,7 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NavController, IonicPage } from 'ionic-angular';
 
-/**
- * Generated class for the FeedbackFormPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-feedback-form',
@@ -14,11 +9,67 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FeedbackFormPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  @ViewChild('signupSlider') signupSlider: any;
+
+  slideOneForm: FormGroup;
+  slideTwoForm: FormGroup;
+  slideThreeForm: FormGroup;
+
+  submitAttempt: boolean = false;
+
+  constructor(public navCtrl: NavController, public formBuilder: FormBuilder) {
+
+    this.slideOneForm = formBuilder.group({
+      date: [(new Date()).toISOString(), Validators.required],
+      location:['',Validators.required],
+      name:['',Validators.required],   
+    });
+    this.slideTwoForm = formBuilder.group({
+      males:['',Validators.required],
+      females:['',Validators.required],
+      activities:['',Validators.required],
+    });
+    this.slideThreeForm = formBuilder.group({
+      comments:[''],
+      shared:[''],
+      tablets:[''],     
+    });
+    // firstName: ['test', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+    // lastName: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+    // age: []
+    // username: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z]*')])],
+    // privacy: ['', Validators.required],
+    // bio: ['']
+
+
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad FeedbackFormPage');
+  next() {
+    this.signupSlider.slideNext();
+  }
+
+  prev() {
+    this.signupSlider.slidePrev();
+  }
+
+  save() {
+    this.submitAttempt = true;
+    let submission = Object.assign({},this.slideOneForm.value, this.slideTwoForm.value, this.slideThreeForm.value);
+    console.log('submission',submission)
+    if (!this.slideOneForm.valid) {
+      this.signupSlider.slideTo(0);
+    }
+    else if (!this.slideTwoForm.valid) {
+      this.signupSlider.slideTo(1);
+    }
+    else if (!this.slideThreeForm.valid) {
+      this.signupSlider.slideTo(2);
+    }
+    else {
+      submission._deviceID='test';
+      console.log("success!")
+    }
+
   }
 
 }
