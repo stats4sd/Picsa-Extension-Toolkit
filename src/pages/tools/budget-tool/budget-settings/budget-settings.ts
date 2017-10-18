ee0ea48ccf8807c2a3ec1a2f8c7d93e4cb6ebf47
CHRIS-DESKTOP\Chris <chris.m.clarke@live.co.uk>
2017-10-18 01:33:34 +0100

    budget settings

--- src/pages/tools/budget-tool/budget-settings/budget-settings.ts
+++ src/pages/tools/budget-tool/budget-settings/budget-settings.ts
@@ -16,13 +16,19 @@ export class BudgetSettingsPage {
   newBudgetSlide = true;
   loadBudgetSlide = false;
   enterprises: any
+  months: any;
+  days: any;
   newBudget = {
-    title:null
+    title: null,
+    periods: { values: [], starting: 'Jan', scale: "months", total: 12 }
   }
   @ViewChild(Slides) slides: Slides;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.enterprises=['crop','livestock','other']
+    this.enterprises = ['crop', 'livestock', 'other']
+    this.months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    this.days = ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun']
+    this.newBudget.periods.values = this.months
   }
 
   ionViewDidLoad() {
@@ -40,10 +46,62 @@ export class BudgetSettingsPage {
     this.loadBudgetSlide = true
     this.slides.update()
     this.slides.slideTo(1, 500);
-    
+
   }
+  nextSlide() {
+    this.slides.slideNext()
+  }
+  calculatePeriod() {
+    console.log('calculating period')
+    let scale = this.newBudget.periods.scale
+    let starting = this.newBudget.periods.starting
+    let total = this.newBudget.periods.total
+    let arr = []
+    if (scale == 'months') { arr = this.calculatePeriodMonths(total, starting) }
+    if (scale == 'weeks') { arr = this.calculatePeriodWeeks(total) }
+    if (scale == 'days') { arr = this.calculatePeriodDays(total, starting) }
+    this.newBudget.periods.values = arr
+  }
+  calculatePeriodWeeks(total) {
+    console.log('total',total)
+    let arr = []
+    for (let i = 1; i <= total; i++) {
+      arr.push('week ' + i)
+    }
+    return arr
+  }
+  calculatePeriodMonths(total, starting) {
+    let array = this.months
+    if (starting) {
+      let startIndex = this.months.indexOf(starting)
+      console.log('start index', startIndex)
+      for (let i = 0; i < startIndex; i++) { array.push(array.shift()) }
+    }
+    if(total>array.length){
+      for(let i=0;i<Math.ceil(total/array.length);i++){
+        array = array.concat(array)
+      }
+    }
+    return array.slice(0,total)
+
+  }
+  calculatePeriodDays(total, starting) {
+    let array = this.days
+    if (starting) {
+      let startIndex = this.days.indexOf(starting)
+      console.log('start index', startIndex)
+      for (let i = 0; i < startIndex; i++) { array.push(array.shift()) }
+    }
+    if(total>array.length){
+      for(let i=0;i<Math.ceil(total/array.length);i++){
+        array = array.concat(array)
+      }
+    }
+    return array.slice(0,total)
+  }
+
   createNewBudget() {
-    
+
   }
 
 }
