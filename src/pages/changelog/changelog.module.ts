4257d78b2bc687701759d0c5617cc457f4a1b078
CHRIS-DESKTOP\Chris <chris.m.clarke@live.co.uk>
2018-07-17 22:58:13 +0100

    update changelog

--- src/pages/changelog/changelog.module.ts
+++ src/pages/changelog/changelog.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { TranslateModule } from "@ngx-translate/core";
 import { IonicPageModule } from "ionic-angular";
+import { YoutubePlayerModule } from "ngx-youtube-player";
 import { ComponentsModule } from "../../components/components.module";
 import { ChangelogPage } from "./changelog";
 
@@ -9,7 +10,8 @@ import { ChangelogPage } from "./changelog";
   imports: [
     IonicPageModule.forChild(ChangelogPage),
     ComponentsModule,
-    TranslateModule.forChild()
+    TranslateModule.forChild(),
+    YoutubePlayerModule
   ]
 })
 export class ChangelogPageModule {}
