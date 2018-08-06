532146135213e95479409fe5137807ca760f8b10
laptop\chris <chris.m.clarke@live.co.uk>
2018-08-06 11:24:27 +0200

    Update changelog, fix floating site select icons for climate tool

--- src/tools/climate-tool/components/site-select/site-select.ts
+++ src/tools/climate-tool/components/site-select/site-select.ts
@@ -136,9 +136,10 @@ const weatherIcon = L.icon({
   // shadowUrl: "assets/img/leaf-shadow.png",
   iconSize: [38, 38], // size of the icon
   shadowSize: [50, 64], // size of the shadow
-  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+  // location given from top-left corner of icon, with right positive x and down positive y
+  iconAnchor: [20, 20], // point of the icon which will correspond to marker's location
   shadowAnchor: [4, 62], // the same for the shadow
-  popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+  popupAnchor: [0, 0] // point from which the popup should open relative to the iconAnchor
 });
 
 function _jsonObjectValues(json: any) {
