9e8f5d419bfc13d5e7b2ea9ba1c2551e14021074
Chris\Laptop <chris.m.clarke@live.co.uk>
2018-10-15 21:45:08 +0200

    budget mock data load and upgrade methods

---
+++ src/tools/budget-tool/mocks/budget.api2.mock.ts
@@ -0,0 +1,213 @@
+export const PB_MOCK_API_2: any = {
+  apiVersion: 2,
+  archived: false,
+  created: "2018-10-15T10:12:47.118Z",
+  data: {
+    "0": {
+      activities: {
+        "land-clearing": {
+          name: "land clearing",
+          id: "land-clearing",
+          isSelected: true
+      ***REMOVED***,
+        ploughing: {
+          name: "ploughing",
+          id: "ploughing",
+          isSelected: true
+      ***REMOVED***
+    ***REMOVED***,
+      inputs: {
+        "tractor-hire": {
+          name: "tractor hire",
+          id: "tractor-hire",
+          quantity: 1,
+          cost: -5000,
+          isSelected: true
+      ***REMOVED***,
+        "labour---paid": {
+          name: "labour - paid",
+          id: "labour---paid",
+          quantity: 1,
+          cost: -500,
+          isSelected: true
+      ***REMOVED***
+    ***REMOVED***,
+      familyLabour: {
+        "family-labour": {
+          name: "family labour",
+          id: "family-labour",
+          people: 2,
+          days: 3,
+          isSelected: true
+      ***REMOVED***
+    ***REMOVED***,
+      outputs: {},
+      produceConsumed: {}
+  ***REMOVED***,
+    "1": {
+      activities: {
+        sowing: {
+          name: "sowing",
+          id: "sowing",
+          isSelected: true
+      ***REMOVED***
+    ***REMOVED***,
+      inputs: {
+        seeds: {
+          name: "seeds",
+          id: "seeds",
+          quantity: 4,
+          cost: -500,
+          isSelected: true
+      ***REMOVED***
+    ***REMOVED***,
+      familyLabour: {
+        "family-labour": {
+          name: "family labour",
+          id: "family-labour",
+          people: 1,
+          days: 2,
+          isSelected: true
+      ***REMOVED***
+    ***REMOVED***
+  ***REMOVED***,
+    "2": {
+      activities: {
+        weeding: {
+          name: "weeding",
+          id: "weeding",
+          isSelected: true
+      ***REMOVED***,
+        "apply-fertiliser": {
+          name: "apply fertiliser",
+          id: "apply-fertiliser",
+          isSelected: true
+      ***REMOVED***
+    ***REMOVED***,
+      inputs: {
+        fertiliser: {
+          name: "fertiliser",
+          id: "fertiliser",
+          quantity: 1,
+          cost: -1000,
+          isSelected: true
+      ***REMOVED***
+    ***REMOVED***,
+      familyLabour: {
+        "family-labour": {
+          name: "family labour",
+          id: "family-labour",
+          people: 2,
+          days: 2,
+          isSelected: true
+      ***REMOVED***
+    ***REMOVED***
+  ***REMOVED***,
+    "3": {
+      activities: {
+        weeding: {
+          name: "weeding",
+          id: "weeding",
+          isSelected: true
+      ***REMOVED***
+    ***REMOVED***,
+      inputs: {},
+      familyLabour: {
+        "family-labour": {
+          name: "family labour",
+          id: "family-labour",
+          people: 2,
+          days: 1,
+          isSelected: true
+      ***REMOVED***
+    ***REMOVED***
+  ***REMOVED***,
+    "4": {
+      activities: {
+        "apply-fertiliser": {
+          name: "apply fertiliser",
+          id: "apply-fertiliser",
+          isSelected: true
+      ***REMOVED***
+    ***REMOVED***,
+      inputs: {
+        fertiliser: {
+          name: "fertiliser",
+          id: "fertiliser",
+          quantity: 1,
+          cost: -500,
+          isSelected: true
+      ***REMOVED***
+    ***REMOVED***,
+      familyLabour: {
+        "family-labour": {
+          name: "family labour",
+          id: "family-labour",
+          people: 1,
+          days: 2,
+          isSelected: true
+      ***REMOVED***
+    ***REMOVED***
+  ***REMOVED***,
+    "5": {
+      activities: {
+        harvesting: {
+          name: "harvesting",
+          id: "harvesting",
+          isSelected: true
+      ***REMOVED***,
+        "marketing-and-selling": {
+          name: "marketing and selling",
+          id: "marketing-and-selling",
+          isSelected: true
+      ***REMOVED***
+    ***REMOVED***,
+      inputs: {
+        "transportation-hire": {
+          name: "transportation hire",
+          id: "transportation-hire",
+          quantity: 1,
+          cost: -2000,
+          isSelected: true
+      ***REMOVED***
+    ***REMOVED***,
+      outputs: {
+        crop: {
+          name: "crop",
+          id: "crop",
+          quantity: 5,
+          cost: 3000,
+          isSelected: true
+      ***REMOVED***
+    ***REMOVED***,
+      familyLabour: {
+        "family-labour": {
+          name: "family labour",
+          id: "family-labour",
+          people: 4,
+          days: 5,
+          isSelected: true
+      ***REMOVED***
+    ***REMOVED***,
+      produceConsumed: {}
+  ***REMOVED***
+***REMOVED***,
+  description: "Example by Chris to test Maize budget",
+  enterprise: null,
+  _key: "_v2_Demo_Maize",
+  periods: {
+    labels: ["Oct", "Nov", "Dec", "Jan", "Feb", "Mar"],
+    starting: "Oct",
+    scale: "months",
+    total: "6"
+***REMOVED***,
+  title: "Maize Demo (v2)",
+  scale: null,
+  enterpriseType: "crop",
+  dotValues: {
+    large: 50000,
+    medium: 10000,
+    small: 1000,
+    half: 500
+***REMOVED***
+***REMOVED***
