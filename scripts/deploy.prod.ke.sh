974c36cecde518d688db27d9e1c407e687fd8c65
CHRIS-DESKTOP\Chris <chris.m.clarke@live.co.uk>
2019-05-28 22:00:04 +0100

    resource scripts and travis

--- scripts/deploy.prod.ke.sh
+++ scripts/deploy.prod.ke.sh
@@ -1,16 +1,7 @@
 # deploy script for kenya.picsa.app domain (ke version)
 firebase use production
 
-# set production environment
-cp ./src/environments/environment.production.ts ./src/environments/environment.ts;
-
-# set kenya resources
-echo 'Preparing Kenya Project Deploy';
-cp ./src/environments/region.ke.ts ./src/environments/region.ts;
-cp ./src/theme/variables.ke.scss ./src/theme/variables.scss;
-
-# add malawi project host
-# firebase target:apply hosting picsa-kenya picsa-kenya
+echo 'Deploy Kenya Version';
 
 # deploy
 firebase deploy --only hosting:picsa-kenya
