fdfea888fab6522c565fd4cec1763e1b91367dd8
CHRIS-DESKTOP\Chris <chris.m.clarke@live.co.uk>
2019-05-28 14:51:20 +0100

    staging, production and region deploy scripts

---
+++ scripts/deploy.dev.sh
@@ -0,0 +1,12 @@
+# standard deploy script for next.picsa.app domain (mw version)
+firebase use staging
+# firebase variables are set using command line on a project-wide basis. Can be viewed with code below
+# echo "firebase conf: $(firebase functions:config:get)"
+# deploy site, functions, rules etc.
+
+# set staging resources
+echo 'Preparing Staging Deploy';
+cp ./src/environments/region.staging.ts ./src/environments/region.ts;
+cp ./src/theme/variables.staging.scss ./src/theme/variables.scss;
+
+firebase deploy
--- scripts/prepare.ke.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-STRING="Preparing Kenya Build"
-echo $STRING;
-cp ./src/environments/region.ke.ts ./src/environments/region.ts;
-cp ./src/theme/variables.ke.scss ./src/theme/variables.scss;
-echo 'Preparation complete'
