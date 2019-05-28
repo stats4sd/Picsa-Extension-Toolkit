3573a3d3648a504d151e795ce6e9d1c7132bc170
CHRIS-DESKTOP\Chris <chris.m.clarke@live.co.uk>
2019-05-28 21:36:22 +0100

    update production env

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,18 +1,26 @@
+const e: IEnv = process.env as any;
+
 ***REMOVED***
   production: false,
 ***REMOVED***
-    apiKey: "AIzaSyCjVzdn5WTQKSDqGw9F0nNbhtdCDcPYP3I",
-    authDomain: "extension-toolkit-staging.firebaseapp.com",
-    databaseURL: "https://extension-toolkit-staging.firebaseio.com",
-    projectId: "extension-toolkit-staging",
-    storageBucket: "extension-toolkit-staging.appspot.com",
-    messagingSenderId: "621985864882",
-    appId: "1:621985864882:web:a19ebe2425f632b6"
+    apiKey: e.FIREBASE_API_KEY,
+    authDomain: e.FIREBASE_AUTH_DOMAIN,
+    databaseURL: e.FIREBASE_DATABASE_URL,
+    projectId: e.FIREBASE_PROJECT_ID,
+    storageBucket: e.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: e.FIREBASE_MESSAGING_SENDER_ID,
+    appId: e.FIREBASE_APP_ID
 ***REMOVED***
 ***REMOVED***
-
-const e = process.env;
 
 // The following variables are programatically added to the production build
 // using travis CI
-interface IEnv {}
+interface IEnv {
+  FIREBASE_API_KEY: string;
+  FIREBASE_AUTH_DOMAIN: string;
+  FIREBASE_DATABASE_URL: string;
+  FIREBASE_PROJECT_ID: string;
+  FIREBASE_STORAGE_BUCKET: string;
+  FIREBASE_MESSAGING_SENDER_ID: string;
+  FIREBASE_APP_ID: string;
+}
