50f85c7c280cd9cb7f3d02cc728ff2697f86408a
CHRIS-DESKTOP\Chris <chris.m.clarke@live.co.uk>
2019-05-29 00:54:11 +0100

    env base variables

--- scripts/prepare.mw.sh
+++ scripts/prepare.mw.sh
@@ -1,6 +1,3 @@
-# set production environment
-cp ./src/environments/environment.production.ts ./src/environments/environment.ts;
-
 # set malawi resources
 echo 'Preparing Malawi Resources';
 cp ./src/environments/region.mw.ts ./src/environments/region.ts;
--- src/environments/base.ts
+++ src/environments/base.ts
@@ -1,22 +1,22 @@
-// As angular can't pick up process.env variables ng-node-environment is used to populate
-// this file
+// As angular can't pick up process.env variables ng-node-environment is used to repopulate
+// this file on production config (see https://github.com/angular/angular-cli/issues/4318)
 
 const env: IEnv = {
-  FIREBASE_API_KEY: "",
-  FIREBASE_AUTH_DOMAIN: "",
-  FIREBASE_DATABASE_URL: "",
-  FIREBASE_PROJECT_ID: "",
-  FIREBASE_STORAGE_BUCKET: "",
-  FIREBASE_MESSAGING_SENDER_ID: "",
-  FIREBASE_APP_ID: ""
+  firebaseApiKey: "AIzaSyCjVzdn5WTQKSDqGw9F0nNbhtdCDcPYP3I",
+  firebaseAuthDomain: "extension-toolkit-staging.firebaseapp.com",
+  firebaseDatabaseUrl: "https://extension-toolkit-staging.firebaseio.com",
+  firebaseProjectId: "extension-toolkit-staging",
+  firebaseStorageBucket: "extension-toolkit-staging.appspot.com",
+  firebaseMessagingSenderId: "621985864882",
+  firebaseAppId: "1:621985864882:web:a19ebe2425f632b6"
 ***REMOVED***
 export default env;
 interface IEnv {
-  FIREBASE_API_KEY: string;
-  FIREBASE_AUTH_DOMAIN: string;
-  FIREBASE_DATABASE_URL: string;
-  FIREBASE_PROJECT_ID: string;
-  FIREBASE_STORAGE_BUCKET: string;
-  FIREBASE_MESSAGING_SENDER_ID: string;
-  FIREBASE_APP_ID: string;
+  firebaseApiKey: string;
+  firebaseAuthDomain: string;
+  firebaseDatabaseUrl: string;
+  firebaseProjectId: string;
+  firebaseStorageBucket: string;
+  firebaseMessagingSenderId: string;
+  firebaseAppId: string;
 }
